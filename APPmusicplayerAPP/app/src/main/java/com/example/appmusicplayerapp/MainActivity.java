2e84a8697b44219757fffa4d97f5c019815d5d45
royishan2004 <123800982+royishan2004@users.noreply.github.com>
2023-08-21 20:04:23 +0530

    Home Page Updated

--- APPmusicplayerAPP/app/src/main/java/com/example/appmusicplayerapp/MainActivity.java
+++ APPmusicplayerAPP/app/src/main/java/com/example/appmusicplayerapp/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.appmusicplayerapp;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button StartButton = findViewById(R.id.start_button);
+        ImageView logo = findViewById(R.id.twlogo);
+        Animation logofadein = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+        logo.startAnimation(logofadein);
+
+        ImageButton StartButton = findViewById(R.id.start_button);
+        Animation butslideinup = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slideinup);
+        StartButton.startAnimation(butslideinup);
         StartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
