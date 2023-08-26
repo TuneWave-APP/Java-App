cf6aaab956fc0c8769f0f7aa5152088d58dc385b
royishan2004 <123800982+royishan2004@users.noreply.github.com>
2023-08-27 01:00:41 +0530

    Added Multi-Song Support

--- APPmusicplayerAPP/app/src/main/java/com/example/appmusicplayerapp/musicplayer.java
+++ APPmusicplayerAPP/app/src/main/java/com/example/appmusicplayerapp/musicplayer.java
@@ -12,6 +12,9 @@ import android.media.AudioAttributes;
 import android.net.Uri;
 import android.os.Handler;
 import android.content.Intent;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.ArrayList;
 
 public class musicplayer extends AppCompatActivity {
     MediaPlayer musicPlayer;
@@ -20,23 +23,19 @@ public class musicplayer extends AppCompatActivity {
     TextView End;
     boolean isSeeking;
     Handler handler = new Handler();
+    int currsongindex = 0;
+    String song[][] = {
+            {"Summer", "Marshmello", String.valueOf(R.drawable.tunewave_logo), String.valueOf(R.raw.marshmello_summer)},
+            {"Alone", "Marshmello", String.valueOf(R.drawable.tunewave_logo), String.valueOf(R.raw.marshmello_alone)}
+    };
+    List<MediaPlayer> mediaPlayerList = new ArrayList<>();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_musicplayer);
 
-        try {
-            musicPlayer = new MediaPlayer();
-            musicPlayer.setAudioAttributes(
-                    new AudioAttributes.Builder()
-                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
-                            .setUsage(AudioAttributes.USAGE_MEDIA)
-                            .build()
-            );
-            //Change below line once database imported
-            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.marshmello_summer);
-            musicPlayer.setDataSource(this, uri);
-            musicPlayer.prepare();
 
+        try {
             ImageButton PlayButton = findViewById(R.id.PlayerPlayButton);
             ImageButton PrevButton = findViewById(R.id.PlayerPrevBut);
             ImageButton NextButton = findViewById(R.id.PlayerNextBut);
@@ -44,9 +43,7 @@ public class musicplayer extends AppCompatActivity {
             Current = findViewById(R.id.PlayerStartTime);
             End = findViewById(R.id.PlayerEndTime);
             isSeeking = false;
-
-            Duration.setMax(musicPlayer.getDuration());
-            Duration.setProgress(musicPlayer.getCurrentPosition());
+            setupmusicplayer();
             PlayButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -59,13 +56,62 @@ public class musicplayer extends AppCompatActivity {
                     }
                 }
             });
+
             setupSeekBarListener();
             updateUIComponents();
             handler.post(updateUIRunnable);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    void setupmusicplayer() {
+        if (mediaPlayerList.size() > currsongindex) {
+            musicPlayer = mediaPlayerList.get(currsongindex);
+        } else {
+            musicPlayer = new MediaPlayer();
+            musicPlayer.setAudioAttributes(
+                    new AudioAttributes.Builder()
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .setUsage(AudioAttributes.USAGE_MEDIA)
+                            .build()
+            );
+            Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + song[currsongindex][3]);
+            try {
+                musicPlayer.setDataSource(this, uri);
+                musicPlayer.prepare();
+                mediaPlayerList.add(musicPlayer); // Add to the list
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        Duration.setMax(musicPlayer.getDuration());
+        Duration.setProgress(musicPlayer.getCurrentPosition());
+
+        musicPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener()
+
+        {
+            @Override
+            public void onCompletion (MediaPlayer mediaPlayer){
+                currsongindex++; // Proceed to the next song
+                if (currsongindex < song.length) {
+                    setupmusicplayer(); // Set up the next song
+                    musicPlayer.start(); // Start playing the next song
+                }
+            }
+        });
+
+        if(musicPlayer !=null)
+
+        {
+            Duration.setMax(musicPlayer.getDuration());
+            Duration.setProgress(musicPlayer.getCurrentPosition());
+        }
+    }
+
+
     Runnable updateUIRunnable = new Runnable() {
         @Override
         public void run() {
